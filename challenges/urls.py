21a32a2f1e925423e43bf425cc0ba09c177ded70
SamiraParsa <misssamiraparsa@gmail.com>
2026-02-15 10:08:05 +0330

    feat: add initial HTML template

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
+    path('', views.days_list),
     path('<int:day>', views.daynamic_day_by_num),
-    path('<str:day>', views.daynamic_days),
+    path('<str:day>', views.daynamic_days , name='days-of-week'),
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse,HttpResponseNotFound,HttpResponseRedirect
+from django.urls import reverse
+from django.template.loader import render_to_string
 
 #create Dictionary
 days = {
@@ -11,19 +13,33 @@ days = {
     'friday': 'this is friday',
 }
 
+def days_list(request):
+    days_list = list(days.keys())
+    list_item = " "
+    for day in days_list :
+        url_path = reverse('days-of-week', args=[day])
+        list_item += f'<li> <a href = "{url_path}">{day} </a> </li>\n'
+    contant = f'<ul>\n {list_item}\n</ul>'
+
+    return HttpResponse(contant)
+
 def daynamic_day_by_num(request , day):
     days_name = list(days.keys())
     if day <= len(days_name):
         redirect_day = days_name[day-1]
-        return HttpResponseRedirect(f'/days/{redirect_day}')
+        redirect_url = reverse('days-of-week', args=[redirect_day])
+        return HttpResponseRedirect(redirect_url)
     else:
         return HttpResponseNotFound("day not exists")
-    #return HttpResponse(day)
 
 
 def daynamic_days(request , day):
     day_data = days.get(day)
     if day_data is not None:
-        return HttpResponse(
-        f'day is : {day} and data is : {day_data}')
+        #response_data = f'<h1 , style = "color :red" >day is : {day} and data is : {day_data}</h1>'
+        response_data = render_to_string('challenges/challenge.html')
+        return HttpResponse(response_data)
     return HttpResponseNotFound('day does not exists')
+
+
+
