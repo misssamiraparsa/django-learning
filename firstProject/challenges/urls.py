98eac8c05721f1ed76b11e1314daca7d6bac5bf5
SamiraParsa <misssamiraparsa@gmail.com>
2026-02-19 01:55:49 +0330

    Restructure project folders

---
+++ firstProject/challenges/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns =[
+    path('', views.days_list , name= 'days_list'),
+    path('<int:day>', views.daynamic_day_by_num),
+    path('<str:day>', views.daynamic_days, name='days-of-week'),
+]
