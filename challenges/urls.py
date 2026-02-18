fef30b30773ed6be411746ad55b6a0dc5c2a1a7d
SamiraParsa <misssamiraparsa@gmail.com>
2026-02-19 02:03:19 +0330

    Restructure project: move challenges app, templates, static files, and manage.py into firstProject folder

--- challenges/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns =[
-    path('', views.days_list , name= 'days_list'),
-    path('<int:day>', views.daynamic_day_by_num),
-    path('<str:day>', views.daynamic_days, name='days-of-week'),
-]
