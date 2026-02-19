04126ed658af01ba5d7d86c854a114cbf49794f2
SamiraParsa <misssamiraparsa@gmail.com>
2026-02-19 12:42:17 +0330

    Create homepage template, add static image, and implement index view

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'blog_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,6 +119,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
