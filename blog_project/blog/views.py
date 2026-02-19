727e160261f68ab0dcd87599850806e15208cd3c
SamiraParsa <misssamiraparsa@gmail.com>
2026-02-19 13:41:35 +0330

    Add post-detail page with template, styling, and view

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -9,6 +9,5 @@ def index(request):
 def posts(request):
     return render(request, 'blog/all-posts.html')
 
-def single_post(request):
-    pass
-
+def single_post(request,slug):
+    return render(request, 'blog/post-detail.html')
