ec7a823f464203d82f1fdad4b4c9fd2cad062a16
SamiraParsa <misssamiraparsa@gmail.com>
2026-02-20 11:55:22 +0330

    Refactor posts view to render dynamic data

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,10 +1,59 @@
 from django.shortcuts import render
+from datetime import date
 
-# Create your views here.
+all_posts = [
+    {
+        'slug': 'learn-django',
+        'title': 'my first django project',
+        'author': 'Samira Parsa',
+        'image': 'django.png',
+        'data': date(2026, 3, 11),
+        'short_description': 'this is my first project:D',
+        'content': """
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid dicta, eius eos eum eveniet
+            perspiciatis quod soluta veritatis? Alias atque ducimus facere odit pariatur! Alias, aspernatur consequuntur
+            deleniti est fugit officiis porro quia saepe tenetur, veniam veritatis voluptatem voluptatum?
+        """
+    },
+
+    {
+        'slug': 'learn-kotlin',
+        'title': 'my first django project',
+        'author': 'zahra Parsa',
+        'image': 'ml.png',
+        'data': date(2026, 3, 12),
+        'short_description': 'this is my first project:D',
+        'content': """
+     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid dicta, eius eos eum eveniet
+         perspiciatis quod soluta veritatis? Alias atque ducimus facere odit pariatur! Alias, aspernatur consequuntur
+         deleniti est fugit officiis porro quia saepe tenetur, veniam veritatis voluptatem voluptatum?
+     """
+    },
+
+    {
+        'slug': 'learn-python',
+        'title': 'my first django project',
+        'author': 'Sima Parsa',
+        'image': 'python.png',
+        'data': date(2026, 3, 4),
+        'short_description': 'this is my first project:D',
+        'content': """
+     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid dicta, eius eos eum eveniet
+         perspiciatis quod soluta veritatis? Alias atque ducimus facere odit pariatur! Alias, aspernatur consequuntur
+         deleniti est fugit officiis porro quia saepe tenetur, veniam veritatis voluptatem voluptatum?
+     """
+    }
+]
+
+def get_date(post):
+    return post['data']
 
 def index(request):
-    return render(request , 'blog/index.html')
-
+    sorted_posts = sorted(all_posts,key=get_date)
+    latest_posts = sorted_posts[-2:]
+    return render(request, 'blog/index.html', {
+        'latest_posts': latest_posts
+    })
 
 def posts(request):
     return render(request, 'blog/all-posts.html')
