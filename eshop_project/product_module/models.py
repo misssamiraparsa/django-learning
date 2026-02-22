7bf06fa6165aa8f17919ea959045c951b4788603
SamiraParsa <misssamiraparsa@gmail.com>
2026-02-22 13:16:31 +0330

    refactor: use get_absolute_url for product links

--- eshop_project/product_module/models.py
+++ eshop_project/product_module/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator,MaxValueValidator
-
+from django.urls import reverse
 # Create your models here.
 
 class Product(models.Model):
@@ -9,6 +9,9 @@ class Product(models.Model):
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)],default=0)
     short_description = models.CharField(max_length=300,null=True)
     is_active = models.BooleanField(default=False)
+
+    def get_absolute_url(self):
+        return reverse('product-detail', args=[self.id])
 
     def __str__(self):
         return f"{self.title}({self.price})"
