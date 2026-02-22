81260c658a6e9c335c0dd9a692be7e71062b7538
SamiraParsa <misssamiraparsa@gmail.com>
2026-02-22 12:07:48 +0330

    feat: add product list and detail views with template rendering

---
+++ eshop_project/product_module/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.product_list),
+    path('<int:product_id>', views.product_detail)
+]
--- eshop_project/product_module/views.py
+++ eshop_project/product_module/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
+
 
 # Create your views here.
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'product_module/product_list.html', {
+        'products': products
+    })
+
+
+def product_detail(request, product_id):
+    #product = Product.objects.get(id=product_id)
+    product = get_object_or_404(Product , pk=product_id )
+    return render(request, 'product_module/product_detail.html', {
+        'product': product
+    })
