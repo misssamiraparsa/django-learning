3bb3877908ef8228af4530ea5a9847af3d57fda2
SamiraParsa <misssamiraparsa@gmail.com>
2026-02-22 19:52:29 +0330

    feat: add product ordering and aggregate statistics

--- eshop_project/product_module/views.py
+++ eshop_project/product_module/views.py
@@ -1,19 +1,26 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
+from django.db.models import Avg
 
 
 # Create your views here.
 
 def product_list(request):
-    products = Product.objects.all()
+    products = Product.objects.all().order_by('-price')
+    number_of_products = products.count()
+    avg_rating = products.aggregate(Avg("rating"))
+
     return render(request, 'product_module/product_list.html', {
-        'products': products
+        'products': products,
+        'total_number_of_products':number_of_products,
+        'average_ratings':avg_rating
+
     })
 
 
 def product_detail(request, slug):
     #product = Product.objects.get(id=product_id)
-    product = get_object_or_404(Product , slug=slug)
+    product = get_object_or_404(Product, slug=slug)
     return render(request, 'product_module/product_detail.html', {
         'product': product
     })
