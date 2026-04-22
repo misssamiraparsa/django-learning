3bacac5df9144707dae5bcc4204854ede57e9ed7
SamiraParsa <misssamiraparsa@gmail.com>
2026-04-22 08:33:05 +0330

    feat: enhance Django forms with widgets and custom attributes

--- eshop_project/contact_module/forms.py
+++ eshop_project/contact_module/forms.py
@@ -1,7 +1,32 @@
 from django import forms
 
+
 class ContactUsForm(forms.Form):
-    full_name = forms.CharField()
-    email = forms.EmailField()
-    subject = forms.CharField()
-    text = forms.CharField()
+    full_name = forms.CharField(label='نام و نام خانوادگی', max_length=50,
+                                error_messages={
+                                    'required': 'please enter your fullname',
+                                },
+                                widget=forms.TextInput(attrs={
+                                    'class': 'form-control',
+                                    'placeholder': 'نام و نام خانوادگی'
+                                })
+                                )
+    email = forms.EmailField(label='ایمیل',
+                             widget=forms.EmailInput(attrs={
+                                 'class': 'form-control',
+                                 'placeholder': 'ایمیل'
+                             })
+                             )
+    subject = forms.CharField(label='موضوع', required=False,
+                              widget=forms.EmailInput(attrs={
+                                  'class': 'form-control',
+                                  'placeholder': 'موضوع'
+                              })
+                              )
+    text = forms.CharField(label='متن',
+                           widget=forms.Textarea(attrs={
+                               'class': 'form-control',
+                               'placeholder': 'متن پیام',
+                               'rows': '5',
+                               'id': 'massage'
+                           }))
--- eshop_project/contact_module/views.py
+++ eshop_project/contact_module/views.py
@@ -10,8 +10,9 @@ def contact_us_page(request):
         if contact_form.is_valid():
             print(contact_form.cleaned_data)
             return redirect('home_page')
+    else:
+        contact_form = ContactUsForm()
 
-    contact_form = ContactUsForm()
     return render(request, 'contact_module/contact_us_page.html', {
         'contact_form': contact_form
     })
