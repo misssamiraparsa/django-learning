93df1ab4acd5b45b05549c127c3ee45219a46445
SamiraParsa <misssamiraparsa@gmail.com>
2026-04-30 21:43:00 +0330

    feat: add CreateProfileView with template and implement file upload

--- eshop_project/contact_module/urls.py
+++ eshop_project/contact_module/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path('', views.ContactUsView.as_view(), name='contact-us-page')
+    path('', views.ContactUsView.as_view(), name='contact-us-page'),
+    path('create-profile/', views.CreateProfileView.as_view(), name='create_profile_page')
 ]
--- eshop_project/contact_module/views.py
+++ eshop_project/contact_module/views.py
@@ -1,25 +1,27 @@
 from django.shortcuts import render, redirect
-from .forms import ContactUsForm, ContactUsModelForm
 from django.views import View
-from django.views.generic.edit import FormView
+
+from .forms import ContactUsForm, ContactUsModelForm
+from django.views.generic.edit import FormView, CreateView
+
+from .models import ContactUs
 
 
-class ContactUsView(FormView):
+class ContactUsView(CreateView):
     template_name = 'contact_module/contact_us_page.html'
     form_class = ContactUsModelForm
 
     success_url = '/contact-us/'
 
-    def form_valid(self, form):
-        form.save()
-        return super().form_valid(form)
 
-# def post(self, request):
+#  def form_valid(self, form):
+#     form.save()
+#    return super().form_valid(form)
 
-#    contact_form = ContactUsModelForm(request.POST)
-#   if contact_form.is_valid():
-#      contact_form.save()
-#     return redirect('home_page')
+class CreateProfileView(View):
+    def get(self, request):
+        return render(request, 'contact_module/create_profile_page.html')
 
-#        return render(request, 'contact_module/contact_us_page.html', {
-#           'contact_form': contact_form})
+    def post(self, request):
+        print(request.FILES)
+        return redirect('/contact-us/create-profile')
