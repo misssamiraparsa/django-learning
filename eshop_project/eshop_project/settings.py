934ae66033314777fc697d774df11f1b54322d73
SamiraParsa <misssamiraparsa@gmail.com>
2026-05-09 09:02:03 +0330

    feat: Implement and fetch data using sessions

--- eshop_project/eshop_project/settings.py
+++ eshop_project/eshop_project/settings.py
@@ -124,3 +124,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#SESSION_COOKIE_AGE = 120
--- eshop_project/product_module/views.py
+++ eshop_project/product_module/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from django.views.generic.base import TemplateView
-from django.views.generic import ListView,DeleteView
+from django.views.generic import ListView, DeleteView
 
 from .models import Product, ProductCategory
 from django.db.models import Avg
@@ -28,6 +28,18 @@ class ProductDetailView(DeleteView):
     template_name = 'product_module/product_detail.html'
     model = Product
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        loaded_product = self.object
+        request = self.request
+        favorite_product_id = request.session.get("product_favorite")
+        context['is_favorite'] = favorite_product_id == str(loaded_product.id)
+        return context
 
 
-
+class AddProductFavorite(View):
+    def post(self, request):
+        product_id = request.POST["product_id"]
+        product = Product.objects.get(pk=product_id)
+        request.session["product_favorite"] = product_id
+        return redirect(product.get_absolute_url())
