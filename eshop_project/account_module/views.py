f986084e897a4f8dbea84f977f2809ca227e4a5b
SamiraParsa <misssamiraparsa@gmail.com>
2026-05-17 12:43:21 +0330

    feat: add user registration with form, view, and template

--- eshop_project/account_module/views.py
+++ eshop_project/account_module/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
 from django.contrib.auth import get_user_model
-# Create your views here.
+from django.views import View
+from .forms import RegisterForm
 
-user = get_user_model()
+
+class RegisterView(View):
+
+    def get(self, request):
+        register_form = RegisterForm()
+        context = {
+            'register_form': register_form
+        }
+        return render(request, 'account_module/register.html', context)
+
+    def post(self, request):
+        register_form = RegisterForm(request.POST)
+        if register_form.is_valid():
+            print(register_form.cleaned_data)
+            pass
+
+        context = {
+            'register_form': register_form
+        }
+        return render(request, 'account_module/register.html', context)
