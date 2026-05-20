1803919de0b06a67783d9e63ec35f88699011150
SamiraParsa <misssamiraparsa@gmail.com>
2026-05-20 14:20:07 +0330

    feat: implement login page and account activation flow

--- eshop_project/account_module/urls.py
+++ eshop_project/account_module/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('register', views.RegisterView.as_view(), name='register_page'),
-    path('login', views.LoginView.as_view(), name='login_page')
+    path('login', views.LoginView.as_view(), name='login_page'),
+    path('activate-account/<email_active_code>',views.ActivateAccountView.as_view(),name='activate_account')
 ]
