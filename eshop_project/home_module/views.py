15ec8591110108cd894e540912a07779b54d6d23
SamiraParsa <misssamiraparsa@gmail.com>
2026-06-05 12:31:50 +0330

    feat: make homepage slider dynamic

--- eshop_project/home_module/views.py
+++ eshop_project/home_module/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic.base import TemplateView
 
-from site_module.models import SiteSetting, FooterLinkBox
+from site_module.models import SiteSetting, FooterLinkBox, Slider
 
 
 class HomeView(TemplateView):
@@ -9,7 +9,8 @@ class HomeView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['data'] = 'this is data'
+        sliders = Slider.objects.filter(is_active=True)
+        context['sliders'] = sliders
         return context
 
 
